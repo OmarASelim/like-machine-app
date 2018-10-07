add79a750bb59797305d68ac5abe3c57535b73d8
Omar AbdEl-Hady <omar@omarabdelhady.me>
2018-10-07 05:00:31 +0200

    Like and unLike functionality

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import moment from "moment";
 import "./Card.scss";
 
+// APIs
+import { likeLink, unLikeLink, deleteLink } from "../../utils/api";
+
 // Material UI
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -11,22 +14,34 @@ import Typography from "@material-ui/core/Typography";
 import CardHeader from "@material-ui/core/CardHeader";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import ShareIcon from "@material-ui/icons/Share";
-import likeLink from '../../utils/api'
+
 
 class CardLink extends Component {
-
+  state = {
+    isLoggedIn: false,
+    isLiked: false
+  };
   likeLink() {
-    console.log("Liking Post");
+    likeLink(this.props.data.id);
+  }
+
+  unLikeLink() {
+    unLikeLink(this.props.data.id);
+  }
+
+  deleteLink() {
+    deleteLink(this.props.data.id);
   }
 
   render() {
-    const { data } = this.props;
+    const { data, isLoggedIn } = this.props;
 
     return (
       <div className="card-component">
         <Card className="card">
-          <a href={data.url} target="_blank">
+          <a href={data.url} target="_blank" rel="noopener noreferrer">
             <CardHeader
               title={data.title}
               subheader={moment(data.created_at).format("DD MMM YYYY")}
@@ -41,14 +56,29 @@ class CardLink extends Component {
           <CardContent>
             <Typography component="p">{data.description}</Typography>
           </CardContent>
+
           <CardActions>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon onClick={this.likeLink} />
-              <span>{data.like_count}</span>
-            </IconButton>
-            <IconButton aria-label="Share">
-              <ShareIcon />
-            </IconButton>
+            {isLoggedIn ? (
+              <div>
+                {data.liked ? (
+                  <IconButton aria-label="Add to favorites">
+                    <FavoriteIcon onClick={this.unLikeLink.bind(this)} />
+                    <span>{data.like_count}</span>
+                  </IconButton>
+                ) : (
+                  <IconButton aria-label="Add to favorites">
+                    <FavoriteBorderIcon onClick={this.likeLink.bind(this)} />
+                    <span>{data.like_count}</span>
+                  </IconButton>
+                )}
+
+                <IconButton aria-label="Share">
+                  <ShareIcon onClick={this.deleteLink.bind(this)} />
+                </IconButton>
+              </div>
+            ) : (
+              ""
+            )}
           </CardActions>
         </Card>
       </div>
