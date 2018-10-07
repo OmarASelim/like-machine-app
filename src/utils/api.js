d1442090e075d74558c0754eeb227854bfed1b09
Omar AbdEl-Hady <omar@omarabdelhady.me>
2018-10-07 04:59:34 +0200

    API functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,31 +1,51 @@
+import _ from "lodash";
 import axios from "axios";
 
 const BASE_URL = "https://likemachine-api.nerdgeschoss.de";
-
-// Testing Purpose
 
 axios.defaults.headers = {
   accept: "*/*"
 };
 axios.defaults.headers.common = {
-  Authorization: "Bearer 00000000-0000-0000-0000-000000000000"
+  Authorization: "Bearer " + localStorage.getItem("token")
 };
 
-function getLinks() {
+export function getLinks() {
   let url = `${BASE_URL}/links`;
   return axios.get(url).then(res => res.data);
 }
 
-function likeLink(facebookToken, id) {
+export function likeLink(id) {
   let url = `${BASE_URL}/links/${id}/like`;
-  return axios.post(url, facebookToken).then(res => res.data);
+  return axios.post(url).then(res => res.data);
 }
 
-function getSessionID(facebookToken) {
+export function unLikeLink(id) {
+  let url = `${BASE_URL}/links/${id}/like`;
+  return axios.delete(url).then(res => res.data);
+}
+
+export function responseFacebook(response) {
   let url = `${BASE_URL}/session`;
-  return axios
-    .post(url, { facebook_token: facebookToken })
-    .then(res => res.data.id);
+  axios
+    .post(url, {
+      facebook_token: response.accessToken
+    })
+    .then(res => {
+      localStorage.setItem("token", res.data.id);
+    });
 }
 
-export default (getLinks, getSessionID);
+export function postLink(postedLink) {
+  let url = `${BASE_URL}/links`;
+  return axios
+    .post(url, {
+      url: postedLink
+    })
+    .then(res => console.log(res.data));
+}
+
+export function deleteLink(id) {
+  let url = `${BASE_URL}/links/${id}`;
+  return axios.delete(url).then(res => console.log(res.data));
+}
