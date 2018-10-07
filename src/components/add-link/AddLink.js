200c0f08f1f1800cb6b301a6d64261592cf7ec9c
Omar AbdEl-Hady <omar@omarabdelhady.me>
2018-10-07 05:00:07 +0200

    Posting Link

--- src/components/add-link/AddLink.js
+++ src/components/add-link/AddLink.js
@@ -4,13 +4,17 @@ import "./AddLink.scss";
 // Material UI
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
-import Typography from "@material-ui/core/Typography";
 import Modal from "@material-ui/core/Modal";
+
+import TextField from "@material-ui/core/TextField";
+import { postLink } from "../../utils/api";
 
 class AddLink extends Component {
   state = {
-    open: false
+    open: false,
+    url: ""
   };
+
   handleOpen = () => {
     this.setState({ open: true });
   };
@@ -18,19 +22,32 @@ class AddLink extends Component {
   handleClose = () => {
     this.setState({ open: false });
   };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { url } = this.state;
+    postLink(url);
+  };
+
   render() {
+    const { url } = this.state;
     return (
       <div className="add-link-component">
-        <Button
-          onClick={this.handleOpen}
-          variant="fab"
-          color="primary"
-          aria-label="Add"
-          className="add-button"
-        >
-          <AddIcon />
-        </Button>
-
+        <div className="feature-discovery">
+          <Button
+            onClick={this.handleOpen}
+            variant="fab"
+            color="primary"
+            aria-label="Add"
+            className="add-button"
+          >
+            <AddIcon />
+          </Button>
+        </div>
         <Modal
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
@@ -39,12 +56,16 @@ class AddLink extends Component {
           className="modal"
         >
           <div className="add-link">
-            <Typography variant="title" id="modal-title">
-              Text in a modal
-            </Typography>
-            <Typography variant="subheading" id="simple-modal-description">
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+            <form noValidate autoComplete="off" onSubmit={this.onSubmit}>
+              <TextField
+                id="standard-name"
+                name="url"
+                label="URL"
+                value={url}
+                onChange={this.onChange}
+                margin="normal"
+              />
+            </form>
           </div>
         </Modal>
       </div>
